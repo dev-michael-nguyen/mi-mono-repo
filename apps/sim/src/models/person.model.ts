9cc159a34a2ccc549be59a38100a5b6557253e78
Michael Nguyen <michael.nguyen@feisystems.com>
2020-10-22 09:25:57 -0400

    Remove sim app

--- apps/sim/src/models/person.model.ts
+++
@@ -1,8 +0,0 @@
-import { MetadataModel } from '@mi-mono-repo/standard/metadata-lib';
-import { SimplePersonNameModel } from './simple-person-name.model';
-
-export class PersonModel extends MetadataModel {
-  $type = 'Person.PersonModel';
-
-  nameModel = new SimplePersonNameModel();
-}
