c96d7d95d822d6434d5ff5960c236af30ee63230
Michael Nguyen <michael.nguyen@feisystems.com>
2020-10-22 23:41:33 -0400

    Add angular material and flex layout package

--- apps/resume/src/app/app.module.ts
+++ apps/resume/src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    BrowserAnimationsModule,
     BrowserModule,
     CommonModule,
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
