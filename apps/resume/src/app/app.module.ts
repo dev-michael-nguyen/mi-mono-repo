105cb8a67a28c1b0143f76b420a19cd075726e53
Michael Nguyen <michael.nguyen@feisystems.com>
2020-11-03 22:45:09 -0500

    Adopt a more modular structure for library

--- apps/resume/src/app/app.module.ts
+++ apps/resume/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ENABLE_SW, InitApplicationServiceProvider, IS_PRODUCTION, OfflineModule, SERVICE_WORKER_CONFIG } from '@mi-mono-repo/mi-angular-ui';
+import { ENABLE_SW, InitApplicationServiceProvider, IS_PRODUCTION, OfflineModule, SERVICE_WORKER_CONFIG } from '@mi-angular-ui/offline';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
--- libs/mi-angular-ui/src/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './lib/offline/offline.module';
-export * from './lib/offline/providers/enable-sw.provider';
-export * from './lib/offline/providers/init-application.provider';
-export * from './lib/offline/providers/is-production.provider';
-export * from './lib/offline/providers/service-worker-config.provider';
