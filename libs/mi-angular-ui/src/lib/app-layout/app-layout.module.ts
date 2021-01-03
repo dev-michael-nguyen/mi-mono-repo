524722af0b007d789fba99922dcda600e7fdc4f5
Michael Nguyen <michael.nguyen@feisystems.com>
2021-01-03 14:27:47 -0500

    Add app-layout module

---
+++ libs/mi-angular-ui/src/lib/app-layout/app-layout.module.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { StickyLayoutModule } from '../sticky-layout/sticky-layout.public-api';
+import { AppLayoutComponent } from './app-layout.component';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    StickyLayoutModule
+  ],
+  declarations: [
+    AppLayoutComponent
+  ],
+  entryComponents: [
+    AppLayoutComponent
+  ],
+  exports: [
+    AppLayoutComponent
+  ],
+})
+export class AppLayoutModule { }
