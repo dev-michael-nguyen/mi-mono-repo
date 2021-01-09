32ae75fb820499cf5101b125861bb28fd67f75bb
Michael Nguyen <michael.nguyen@feisystems.com>
2021-01-08 23:26:56 -0500

    Update storybook template with custom theme picker

--- apps/mi-angular-ui-storybook/src/app/app.component.ts
+++ apps/mi-angular-ui-storybook/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'mi-angular-ui-storybook';
+  isDark = false;
 }
--- apps/mi-angular-ui-storybook/src/app/app.module.ts
+++ apps/mi-angular-ui-storybook/src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppLayoutModule } from '@mi-angular-ui';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
+    AppLayoutModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent],
