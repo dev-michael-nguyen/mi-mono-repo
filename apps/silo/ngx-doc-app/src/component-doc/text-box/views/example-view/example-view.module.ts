fc4b629006f6edcba6367f2acb55129f7c82e643
Michael Nguyen <michael.nguyen@feisystems.com>
2021-02-13 16:13:44 -0500

    Add more text box examples

--- apps/silo/ngx-doc-app/src/component-doc/text-box/views/example-view/example-view.module.ts
+++ apps/silo/ngx-doc-app/src/component-doc/text-box/views/example-view/example-view.module.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { TextBoxModule } from '@silo/ngx';
+import { ExampleArticleModule } from '../../../../components/example-article/example-article.module';
 import { ExampleViewComponent } from './example-view.component';
 
 @NgModule({
   imports: [
     CommonModule,
+    ExampleArticleModule,
     TextBoxModule
   ],
   declarations: [ExampleViewComponent]
