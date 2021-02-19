f10238983d109ad4361f3f0ac84f47a790084c04
Michael Nguyen <michael.nguyen@feisystems.com>
2021-02-18 23:51:13 -0500

    Implement auto focus directive

---
+++ libs/silo/ngx-lib/src/directives/auto-focus/auto-focus.directive.spec.ts
@@ -0,0 +1,74 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { AutoFocusDirective } from './auto-focus.directive';
+
+@Component({
+  template: `
+  <div #selfDiv 
+       [attr.tabIndex]="tabIndex" 
+       [siloAutoFocus]="enableAutoFocus"
+       [focusChildSelectors]="focusChildSelectors">
+    <button #button>Test</button>
+    <div #targetIdDiv id="targetId"></div>
+    <div #targetClassDiv class="targetClass"></div>
+  </div>
+  `
+})
+class TestComponent {
+  tabIndex = -1;
+  enableAutoFocus = true;
+  focusChildSelectors = '';
+  @ViewChild(AutoFocusDirective) directive: AutoFocusDirective;
+  @ViewChild('selfDiv') selfDiv: ElementRef<HTMLElement>;
+  @ViewChild('button') button: ElementRef<HTMLElement>;
+  @ViewChild('targetIdDiv') targetIdDiv: ElementRef<HTMLElement>;
+  @ViewChild('targetClassDiv') targetClassDiv: ElementRef<HTMLElement>;
+}
+
+describe('siloAutoFocus', () => {
+  let fixture: ComponentFixture<TestComponent>;
+  let component: TestComponent;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AutoFocusDirective,
+        TestComponent
+      ]
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(TestComponent);
+    component = fixture.componentInstance;
+  });
+
+  it('should auto focus self when self is focusable', () => {
+    component.tabIndex = 0;
+    fixture.detectChanges();
+    expect(component.directive.lastFocusElement).toBe(component.selfDiv.nativeElement);
+  });
+
+  it('should auto focus first button', () => {
+    fixture.detectChanges();
+    expect(component.directive.lastFocusElement).toBe(component.button.nativeElement);
+  });
+
+  it('should do nothing if it is disabled', () => {
+    component.enableAutoFocus = false;
+    fixture.detectChanges();
+    expect(component.directive.lastFocusElement).toBeFalsy();
+  });
+
+  it('should auto focus first element with id "targetId"', () => {
+    component.focusChildSelectors = '#targetId';
+    fixture.detectChanges();
+    expect(component.directive.lastFocusElement).toBe(component.targetIdDiv.nativeElement);
+  });
+
+  it('should auto focus first element with class "targetClass"', () => {
+    component.focusChildSelectors = '.targetClass';
+    fixture.detectChanges();
+    expect(component.directive.lastFocusElement).toBe(component.targetClassDiv.nativeElement);
+  });
+});
---
+++ libs/silo/ngx-lib/src/directives/auto-focus/auto-focus.directive.ts
@@ -0,0 +1,81 @@
+import { AfterViewInit, Directive, ElementRef, Input } from '@angular/core';
+
+/**
+ * @description
+ * This directive will auto focus HTMLElement that it is on when element is first rendered. 
+ * 
+ * @usageNotes
+ * ```html
+ * <!-- auto focus first focusable child in this div -->
+ * <div plrAutoFocus>...<div> 
+ * 
+ * <!-- auto focus this div since its focusable -->
+ * <div plrAutoFocus tabindex=0>...</div>
+ * 
+ * <!-- conditionally enable/disable this directive -->
+ * <div [prlAutoFocus]="isEnable">...</div>
+ * 
+ * <!-- auto focus first element with id or class "header" -->
+ * <div plrAutoFocus focusChildSelectors=".header, #header">...</div>
+ * ```
+ */
+@Directive({
+  selector: '[siloAutoFocus]'
+})
+export class AutoFocusDirective implements AfterViewInit {
+
+  private readonly DEFAULT_SELECTORS = '[tabindex]:not([tabindex="-1"]), button';
+
+  /**
+   * Reference to last focus element.
+   */
+  lastFocusElement: HTMLElement;
+
+  /**
+   * Indicate whether this directive is enable.
+   * Note: If nothing is bind, this will be empty string which will be treated as enable anyway.
+   */
+  @Input('siloAutoFocus')
+  isEnable = true;
+
+  /**
+   * First child that match the specified group of selectors will be focus.
+   * 
+   * {@link https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll}
+   */
+  @Input()
+  focusChildSelectors: string;
+
+  constructor(
+    private _el: ElementRef<HTMLElement>
+  ) { }
+
+  ngAfterViewInit() {
+    this.focusFirstFocusable(this.focusChildSelectors || this.DEFAULT_SELECTORS);
+  }
+
+  /**
+   * Focus first child element that match the specified group of selectors.
+   */
+  focusFirstFocusable(selectors: string) {
+    if (this.isEnable === false) {
+      return;
+    }
+
+    // self focus
+    if (this._el.nativeElement.tabIndex === 0 && !this.focusChildSelectors) {
+      this._el.nativeElement.focus();
+      this.lastFocusElement = this._el.nativeElement;
+      return;
+    }
+
+    const focusable = this._el.nativeElement.querySelectorAll(selectors);
+    if (focusable.length) {
+      const firstFocusable = focusable[0] as HTMLElement;
+      firstFocusable.focus();
+      this.lastFocusElement = firstFocusable;
+      return;
+    }
+  }
+
+}
