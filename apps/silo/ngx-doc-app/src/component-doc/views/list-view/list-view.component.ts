95e71e26ac143bc26e6bb980e4c9375587ccf8b0
Michael Nguyen <dev.michael.nguyen@gmail.com>
2021-02-23 11:21:16 -0500

    Update list view styling

--- apps/silo/ngx-doc-app/src/component-doc/views/list-view/list-view.component.ts
+++ apps/silo/ngx-doc-app/src/component-doc/views/list-view/list-view.component.ts
@@ -4,18 +4,15 @@ import { INavListItemModel } from '@silo/ngx';
 @Component({
   selector: 'silo-list-view',
   templateUrl: './list-view.component.html',
-  styleUrls: ['./list-view.component.scss']
+  styleUrls: ['./list-view.component.scss'],
 })
 export class ListViewComponent implements OnInit {
-
   componentNavList: Array<INavListItemModel> = [
     { label: 'Text Box', routerLink: 'text-box' },
-    { label: 'Text Area', routerLink: 'text-area' }
+    { label: 'Text Area', routerLink: 'text-area' },
   ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
