41340103162c383c04fbd4c785268c9608893316
Michael Nguyen <dev.michael.nguyen@gmail.com>
2021-03-05 15:51:37 -0500

    Only need to run unit tests for @silo/ngx

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,3 @@
 module.exports = {
-  projects: [
-    '<rootDir>/apps/resume',
-    '<rootDir>/apps/silo/ngx-doc-app',
-    '<rootDir>/libs/mi-angular-ui',
-    '<rootDir>/libs/silo/ngx-lib',
-    '<rootDir>/libs/silo/metadata-lib',
-  ],
+  projects: ['<rootDir>/libs/silo/ngx-lib', '<rootDir>/libs/silo/metadata-lib'],
 };
