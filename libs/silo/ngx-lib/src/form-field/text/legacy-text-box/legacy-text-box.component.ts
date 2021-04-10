92d21f3416654c8d2b145467e0acebfee09e50cc
Michael Nguyen <dev.michael.nguyen@gmail.com>
2021-04-10 13:25:28 -0400

    Remove legacy text box and text area

--- libs/silo/ngx-lib/src/form-field/text/legacy-text-box/legacy-text-box.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { TextFieldComponent } from './../text-field.component';
-
-@Component({
-  selector: 'silo-legacy-text-box',
-  templateUrl: './legacy-text-box.component.html',
-  styleUrls: ['./legacy-text-box.component.scss'],
-})
-export class LegacyTextBoxComponent extends TextFieldComponent {}
