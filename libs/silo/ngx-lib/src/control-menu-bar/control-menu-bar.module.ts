59da77243dc588d1e247fce963c8250f460cc063
Michael Nguyen <dev.michael.nguyen@gmail.com>
2021-04-13 18:05:46 -0400

    Add tooltip for control menu bar

--- libs/silo/ngx-lib/src/control-menu-bar/control-menu-bar.module.ts
+++ libs/silo/ngx-lib/src/control-menu-bar/control-menu-bar.module.ts
@@ -3,11 +3,18 @@ import { NgModule } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { ControlMenuBarButtonDirective } from './control-menu-bar-button.directive';
 import { ControlMenuBarComponent } from './control-menu-bar.component';
 
 @NgModule({
-  imports: [CommonModule, MatToolbarModule, MatMenuModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatTooltipModule,
+  ],
   declarations: [ControlMenuBarComponent, ControlMenuBarButtonDirective],
   exports: [ControlMenuBarComponent, ControlMenuBarButtonDirective],
 })
