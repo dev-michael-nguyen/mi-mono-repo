020c6017e894f6e5e37a6941650187506f18e3b6
Michael Nguyen <dev.michael.nguyen@gmail.com>
2021-06-04 09:30:28 -0400

    Responsive container should update when class expression has changed

--- libs/silo/ngx-lib/src/responsive/responsive-container/responsive-container.directive.ts
+++ libs/silo/ngx-lib/src/responsive/responsive-container/responsive-container.directive.ts
@@ -1,44 +1,87 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { ClassExpression } from './models/class-expression';
 
 @Directive({
   selector: '[siloResponsiveContainer]',
 })
 export class ResponsiveContainerDirective implements OnInit {
-  static readonly REPONSIVE_CONTAINER_CLASS = 'silo-responsive-container';
+  static readonly RESPONSIVE_CONTAINER_CLASS = 'silo-responsive-container';
 
   @Input('siloResponsiveContainer')
-  colExpression: ClassExpression;
+  classExpression: ClassExpression;
 
   constructor(private _elementRef: ElementRef<HTMLElement>) {}
 
   ngOnInit() {
-    this.addColExpression(this.colExpression);
+    this.addClassExpression(this.classExpression);
   }
 
-  addColExpression(colExpression: ClassExpression) {
-    if (!colExpression) {
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes.classExpression && !changes.classExpression.isFirstChange()) {
+      this.removeClassExpression(changes.classExpression.previousValue);
+      this.addClassExpression(changes.classExpression.currentValue);
+    }
+  }
+
+  addClassExpression(classExpression: ClassExpression) {
+    if (!classExpression) {
       return;
     }
 
     this._elementRef.nativeElement.classList.add(
-      ResponsiveContainerDirective.REPONSIVE_CONTAINER_CLASS,
+      ResponsiveContainerDirective.RESPONSIVE_CONTAINER_CLASS,
     );
 
-    if (Array.isArray(colExpression)) {
-      this._elementRef.nativeElement.classList.add(...colExpression);
+    if (Array.isArray(classExpression)) {
+      this._elementRef.nativeElement.classList.add(...classExpression);
       return;
     }
 
-    if (typeof colExpression === 'string') {
-      this._elementRef.nativeElement.classList.add(colExpression as string);
+    if (typeof classExpression === 'string') {
+      this._elementRef.nativeElement.classList.add(classExpression as string);
       return;
     }
 
-    if (typeof colExpression === 'object') {
-      Object.keys(colExpression).forEach((key) => {
-        if (colExpression[key]) {
+    if (typeof classExpression === 'object') {
+      Object.keys(classExpression).forEach((key) => {
+        if (
+          classExpression[key] &&
+          !this._elementRef.nativeElement.classList.contains(key)
+        ) {
           this._elementRef.nativeElement.classList.add(key);
+        }
+      });
+      return;
+    }
+  }
+
+  removeClassExpression(classExpression: ClassExpression) {
+    if (!classExpression) {
+      return;
+    }
+
+    if (Array.isArray(classExpression)) {
+      this._elementRef.nativeElement.classList.remove(...classExpression);
+      return;
+    }
+
+    if (typeof classExpression === 'string') {
+      this._elementRef.nativeElement.classList.remove(
+        classExpression as string,
+      );
+      return;
+    }
+
+    if (typeof classExpression === 'object') {
+      Object.keys(classExpression).forEach((key) => {
+        if (this._elementRef.nativeElement.classList.contains(key)) {
+          this._elementRef.nativeElement.classList.remove(key);
         }
       });
       return;
